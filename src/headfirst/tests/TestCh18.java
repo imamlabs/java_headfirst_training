e53913c80c5ce5662f795b841b8d9fc6fc50d9be
Imam Fauzy <itlabscenter@gmail.com>
2025-09-27 08:39:05 +0700

    rev test

--- src/headfirst/tests/TestCh18.java
+++ src/headfirst/tests/TestCh18.java
@@ -1,16 +1,26 @@
 package headfirst.tests;
+
 public class TestCh18 {
     public static void main(String[] args) throws Exception {
-        // Jalankan server RMI pada thread terpisah
+        // Jalankan server RMI pada thread terpisah (daemon)
         Thread t = new Thread(() -> {
-            try { headfirst.ch18.HelloServer.main(new String[0]); } catch (Exception e) { e.printStackTrace(); }
+            try {
+                headfirst.ch18.HelloServer.main(new String[0]);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         });
         t.setDaemon(true);
         t.start();
-        Thread.sleep(500); // Tunggu registry & server bind
+
+        // Tunggu registry & server bind
+        Thread.sleep(500);
 
         // Jalankan client
         headfirst.ch18.HelloClient.main(new String[0]);
         System.out.println("Ch18 OK");
+
+        // >>> Penting: paksa proses selesai supaya thread RMI/registry tidak menggantung
+        System.exit(0);
     }
 }
